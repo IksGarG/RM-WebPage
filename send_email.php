a77767204603116fd621fd39d9e87bed523fa1a2
Iker Garcia <A01782767@tec.mx>
2023-05-29 23:16:54 -0600

    Php Updated

--- send_email.php
+++ send_email.php
@@ -4,13 +4,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $email = $_POST["email"];
   $message = $_POST["message"];
 
-  $to = "reki0103@gmail.com"; // Replace with your own email address
+  // Set the recipient email address
+  $recipient = "cancun2rent@gmail.com";
+
+  // Set the email subject
   $subject = "New Contact Form Submission";
+
+  // Set the email body
   $body = "Name: $name\nEmail: $email\nMessage: $message";
 
+  // Set the headers
+  $headers = "From: $email\r\n";
+  $headers .= "Reply-To: $email\r\n";
+
   // Send the email
-  if (mail($to, $subject, $body)) {
-    echo "Thank you for your message. We will be in touch soon!";
+  if (mail($recipient, $subject, $body, $headers)) {
+    echo "Email sent successfully!";
   } else {
     echo "Oops! Something went wrong. Please try again later.";
   }
